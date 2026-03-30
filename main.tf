64283f9b356ff14eb4e8cb4333670c338c096f71
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2026-03-30 11:18:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  cloud {
+    organization = "april2026"
+
+    workspaces {
+      name = "prod2026"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "bucket01" {
+  bucket = "my-s3-bucket-march-30-2026-9090"
+
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
